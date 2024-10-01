2f27e643d1fe3f2cb4ff26703f5dd0bc589ae775
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-10-01 14:49:34 +0530

    Refactor file paths for static file serving and update environment variables

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -29,16 +52,14 @@ const userSchema_1 = require("../schemas/userSchema");
 const multer_1 = __importDefault(require("multer"));
 const cloudinary_1 = require("cloudinary");
 const fs_1 = __importDefault(require("fs"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 cloudinary_1.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
 });
-console.log({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
+console.log(process.env.CLOUD_NAME);
 const upload = (0, multer_1.default)({ dest: "uploads/" });
 const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
