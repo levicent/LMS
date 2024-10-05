b933aabba8e3d61abb7d2f42ac8dc40b6d6b3c95
Saketh Yalamanchili <saketh.engineer@gmail.com>
2024-10-05 13:20:40 +0530

    Update header, CreateCourseTable, TeacherLayout, InstructorDashboard Add Mobile Responsiveness

--- src/layout/TeacherLayout.tsx
+++ src/layout/TeacherLayout.tsx
@@ -12,29 +12,29 @@ const TeacherLayout = ({ children }: AdminLayoutProps) => {
     <div className="flex h-screen overflow-hidden">
       {/* <!-- ===== Page Wrapper Start ===== --> */}
       {/* <div className="flex h-screen overflow-hidden"> */}
-        {/* <!-- ===== Sidebar Start ===== --> */}
-        <Sidebar1 sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
+      {/* <!-- ===== Sidebar Start ===== --> */}
+      <Sidebar1 sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      {/* <!-- ===== Sidebar End ===== --> */}
 
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div
+      {/* <!-- ===== Content Area Start ===== --> */}
+      <div
         className={`relative flex flex-1 flex-col transition-all duration-300 ${
           sidebarOpen ? "ml-64" : "ml-0"
-        }`}  // Adjusts content based on sidebar state
+        }`} // Adjusts content based on sidebar state
       >
-          {/* <!-- ===== Header Start ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
+        {/* <!-- ===== Header Start ===== --> */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* <!-- ===== Header End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
-          <main className="flex-1 overflow-y-auto bg-gray-100">
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
-        </div>
-        {/* <!-- ===== Content Area End ===== --> */}
+        {/* <!-- ===== Main Content Start ===== --> */}
+        <main className="flex-1 overflow-y-auto bg-gray-100">
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </main>
+        {/* <!-- ===== Main Content End ===== --> */}
+      </div>
+      {/* <!-- ===== Content Area End ===== --> */}
       {/* </div> */}
       {/* <!-- ===== Page Wrapper End ===== --> */}
     </div>
--- src/pages/Dashboard/InstructorDashboard.tsx
+++ src/pages/Dashboard/InstructorDashboard.tsx
@@ -1,9 +1,10 @@
 import TeacherLayout from "../../layout/TeacherLayout";
 import CreateOrEditCourseForm from "@/components/Tables/CourseTable/CreateCourseTable";
+
 const InstructorDashboard = () => {
   return (
     <TeacherLayout>
-      <div className="col-span-12 xl:col-span-8">
+      <div className="w-full">
         <CreateOrEditCourseForm />
       </div>
     </TeacherLayout>
