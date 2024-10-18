4d9077938465257e2769e23d316b4471cf3357e0
mnz02 <abdulminhaz47@gmail.com>
2024-10-18 16:19:51 +0530

    Refactor ProtectedRoutes component to handle loading state and redirect to sign-in page if not authenticated

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "@/context/authContext";
 import { toast } from "react-toastify";
@@ -9,12 +9,24 @@ interface PrivateRoutesProps {
 
 function ProtectedRoutes({ element }: PrivateRoutesProps) {
   const authContext = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authContext) {
+      setIsLoading(false);
+    }
+  }, [authContext]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!authContext?.isAuthenticated) {
     toast.error("Please sign in to view this page");
+    return <Navigate to="/signin" />;
   }
 
-  return authContext?.isAuthenticated ? element : <Navigate to="/signin" />;
+  return <>{element}</>;
 }
 
 export default ProtectedRoutes;
