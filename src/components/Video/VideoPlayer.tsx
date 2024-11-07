9b6e9a535c07d5bfdb6b9745ac88959cbfb8e72e
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-11-07 16:29:56 +0530

    Enhance VideoPlayer component: Add close icon and improve backdrop styling

--- src/components/Video/VideoPlayer.tsx
+++ src/components/Video/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react';
 import React from 'react';
 import ReactPlayer from 'react-player';
 
@@ -9,13 +10,14 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl, videoTitle, onClose }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
       <div className="bg-white rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2">
         <div className="flex justify-between items-center p-4 border-b">
           <h2 className="text-xl font-semibold">{videoTitle}</h2>
-          <button onClick={onClose} className="text-gray-600 hover:text-gray-800">
+          <X onClick={onClose} className='cursor-pointer text-gray-900'/>
+          {/* <button onClick={onClose} className="text-gray-600 hover:text-gray-800 h-8 w-8">
             &times;
-          </button>
+          </button> */}
         </div>
         <div className="p-4">
           <ReactPlayer url={videoUrl} controls width="100%" height="100%" />
