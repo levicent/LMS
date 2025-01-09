24ded5c2799df499ddf0e9ad9d7932fbc9e04db0
mnz02 <abdulminhaz47@gmail.com>
2025-01-09 13:30:47 +0530

    Add url to allowed origins

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import videoRoutes from "./routes/videoRoutes";
 const app = express();
 
 const allowedOrigins = [
-  "https://lms-1-0v55.onrender.com",
+  "https://lms-r6ncqtcjs-levicents-projects.vercel.app/",
   "http://localhost:5173",
 ];
 
