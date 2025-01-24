8726e0cb1acc9ccd32ab5cfb2aa57d88cff095af
mnz02 <abdulminhaz47@gmail.com>
2025-01-24 13:41:57 +0530

    Fix get course by user Id

--- src/controllers/refreshToken.ts
+++ src/controllers/refreshToken.ts
@@ -1,45 +1,44 @@
-import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import User from "../models/User";
+import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
 
 const generateAccessToken = (user: { id: string; role: string }) => {
   return jwt.sign(
     {
       id: user.id,
-      role: user.role,
+      role: user.role
     },
     process.env.JWT_SECRET_KEY as string,
     {
-      expiresIn: "15m",
+      expiresIn: '15m'
     }
-  );
-};
+  )
+}
 export const refreshToken = async (req: Request, res: Response) => {
-  const { refreshToken } = req.cookies;
+  const { refreshToken } = req.cookies
 
   if (!refreshToken) {
-    return res.status(401).json({ message: "Refresh token is required" });
+    return res.status(401).json({ message: 'Refresh token is required' })
   }
-  console.log({ refreshToken } + "saas");
 
   try {
     const decoded = jwt.verify(
       refreshToken,
       process.env.JWT_REFRESH_KEY as string
-    ) as jwt.JwtPayload;
+    ) as jwt.JwtPayload
 
-    const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.id)
     if (!user || user.refreshToken !== refreshToken) {
-      res.clearCookie("refreshToken");
+      res.clearCookie('refreshToken')
       return res
         .status(401)
-        .json({ message: "Invalid or expired refresh token" });
+        .json({ message: 'Invalid or expired refresh token' })
     }
 
     const accessToken = generateAccessToken({
       id: user._id as string,
-      role: user.role,
-    });
+      role: user.role
+    })
 
     // const newRefreshToken = generateRefreshToken({
     //   id: user._id as string,
@@ -49,10 +48,10 @@ export const refreshToken = async (req: Request, res: Response) => {
     // user.refreshToken = newRefreshToken;
     // await user.save();
 
-    res.status(200).json({ accessToken: accessToken });
+    res.status(200).json({ accessToken: accessToken })
   } catch (error) {
-    console.error("Error refreshing token: ", error);
-    res.clearCookie("refreshToken");
-    res.status(500).json({ message: "Invalid or expired refresh token" });
+    console.error('Error refreshing token: ', error)
+    res.clearCookie('refreshToken')
+    res.status(500).json({ message: 'Invalid or expired refresh token' })
   }
-};
+}
